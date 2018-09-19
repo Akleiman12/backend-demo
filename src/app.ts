50c12e1f8083c3ed608dae719967bb38dc0de91d
akleiman12 <asherkleiman@gmail.com>
2018-09-18 22:47:28 -0400

    Basic config and app.ts added

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import * as cors from 'cors'
+
+class App {
+  public express;
+  public router;
+
+  constructor () {
+    //Se declara express, que manejara la comunicación HTTP
+    this.express = express();
+
+    // Se utiliza BodyParser() para poder recibir data de los POSTS
+
+      //Para recibir data del tipo application/x-www-form-urlencoded
+      this.express.use(bodyParser.urlencoded({ extended: true }));
+
+      //Para recibir data del tipo application/json
+      this.express.use(bodyParser.json());
+
+      this.express.use(cors());
+  }
+
+
+}
+
+export default new App().express
